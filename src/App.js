7ecf3d0b8a81e747451dca9c2952dbab44143e6e
aynerd <olawaleayowande@gmail.com>
2023-04-04 09:44:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import { Box } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ExerciseDetails from './pages/ExerciseDetails'
+import Navbar from './component/Navbar'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <Box width="400px" sx={{width:{xl: '1488px'}}}>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/exercies:id'element={<ExerciseDetails/>} />
+    </Routes>
+
+    <Footer/>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/component/Exercises.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import { Pagination } from '@mui/material/Pagination';
+import {Box, Typography, Stack} from "@mui/material"
+import { exerciseOptions, fetchData } from '../Utils/fetchData';
+
+const Exercises = ({exercises,setExercises, bodyPart}) => {
+      return (
+            <div>
+                  Exercises
+            </div>
+      );
+};
+
+export default Exercises;
